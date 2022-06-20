ab8b55062cd38846b30dcf77fd65a4a554b5aa85
praveen041987 <85856288+praveen041987@users.noreply.github.com>
2022-06-20 12:16:25 +0530

    Update a.sh

--- a.sh
+++ a.sh
@@ -10,5 +10,5 @@ then
 else
         echo "this is bad server"  >> /tmp/praveen/script.out
 	echo "this is for auto trigger" >> /tmp/praveen/script.out
-	echo "this is for webhook trigger" >> /tmp/praveen/script.out
+	echo "this is for webhook trigger edited" >> /tmp/praveen/script.out
 fi
