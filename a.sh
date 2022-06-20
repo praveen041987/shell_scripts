03e8a3a755907a1172f0699c55e8121ddc9ad63f
EC2 Default User <ec2-user@ip-172-31-80-234.ec2.internal>
2022-06-20 05:57:36 +0000

    a.sh updated

--- a.sh
+++ a.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-server = `echo $name`
+server=`echo $name`
 
-if [ server == 'APP' ];
+if [ $server == 'APP' ];
 then
         echo "this is APP server" >> /tmp/praveen/script.out
-elif    [ server == 'RPT' ];
+elif    [ $server == 'RPT' ];
 then
         echo "this os for RPT server" >> /tmp/praveen/script.out
 else
